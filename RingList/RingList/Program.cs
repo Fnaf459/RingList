c3928e93850b577132e3763e3fe7b15619150933
sasha <sasha.savenkov.2002@mail.ru>
2024-05-28 22:47:54 +0400

    Ready_2

--- RingList/RingList/Program.cs
+++ RingList/RingList/Program.cs
@@ -21,7 +21,6 @@ class CircularLinkedList
         tail = null;
     }
 
-    // Добавление элемента в середину списка
     public void InsertInMiddle(int data)
     {
         Node newNode = new Node(data);
@@ -35,7 +34,6 @@ class CircularLinkedList
         {
             Node slow = tail.Next, fast = tail.Next;
 
-            // Найти середину списка
             while (fast != tail && fast.Next != tail)
             {
                 slow = slow.Next;
@@ -45,7 +43,6 @@ class CircularLinkedList
             newNode.Next = slow.Next;
             slow.Next = newNode;
 
-            // Если вставка происходит после tail, нужно обновить tail
             if (slow == tail)
             {
                 tail = newNode;
@@ -53,7 +50,6 @@ class CircularLinkedList
         }
     }
 
-    // Удаление элемента по значению
     public void DeleteByValue(int value)
     {
         if (tail == null) return;
@@ -67,7 +63,6 @@ class CircularLinkedList
             {
                 if (current == tail.Next && current == tail)
                 {
-                    // Единственный элемент в списке
                     tail = null;
                 }
                 else
@@ -85,7 +80,6 @@ class CircularLinkedList
         } while (current != tail.Next);
     }
 
-    // Вывод списка на экран
     public void Display()
     {
         if (tail == null)
@@ -107,7 +101,6 @@ class CircularLinkedList
     {
         CircularLinkedList list = new CircularLinkedList();
 
-        // Добавление элементов в середину
         list.InsertInMiddle(10);
         Console.WriteLine("Список после вставки:");
         list.Display();
@@ -124,17 +117,14 @@ class CircularLinkedList
         Console.WriteLine("Список после вставки:");
         list.Display();
 
-        // Удаление элемента
         list.DeleteByValue(30);
         Console.WriteLine("Список после удаления 30:");
         list.Display();
 
-        // Удаление элемента, который отсутствует
         list.DeleteByValue(50);
         Console.WriteLine("Список после попытки удаления 50:");
         list.Display();
 
-        // Удаление оставшихся элементов
         list.DeleteByValue(10);
         list.DeleteByValue(20);
         list.DeleteByValue(40);
